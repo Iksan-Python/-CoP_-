a9122c812bdb2b1429b353b2f0bb0670c684a66c
heuiy <52515917+heuiy@users.noreply.github.com>
2021-05-28 15:28:02 +0900

    Update and rename Module_01/Day_05_210525_화/repeat.py to Module_01/Day_05_210601_화/Quiz.py

---
+++ Module_01/Day_05_210601_화/Quiz.py
@@ -0,0 +1,10 @@
+
+df1 = pd.read_csv("https://raw.githubusercontent.com/heuiy/data/main/titanic/gender_submission.csv")
+
+df2 = pd.read_csv("https://raw.githubusercontent.com/heuiy/data/main/titanic/train.csv")
+
+df3 = pd.read_csv("https://raw.githubusercontent.com/heuiy/data/main/titanic/test.csv")
+
+df4 = pd.read_csv("https://raw.githubusercontent.com/heuiy/data/main/weather/weather.csv")
+
+df = pd.read_csv("https://raw.githubusercontent.com/heuiy/data/main/AICamp/titanic.csv")
